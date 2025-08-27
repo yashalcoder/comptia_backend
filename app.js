b4535de834e5929607db50f0b774b47514cc09b2
FatimaSami <84532632+fatima-Sami55@users.noreply.github.com>
2025-08-27 21:44:29 +0500

    commiting file structure

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
