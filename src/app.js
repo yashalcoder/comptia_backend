4577f10f4bdddb2755f5ee3bc8fe80ceaa395750
yashalcoder <yashalrafique@gmail.com>
2025-09-10 09:03:43 +0500

    testing

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const authRoutes = require("./routes/auth.js");
 const app = express();
 app.use(express.json());
 
+// ✅ Health check route (for testing deployment)
+app.get("/", (req, res) => {
+  res.send("🚀 Backend successfully deployed on Vercel!");
+});
 //routes
 app.use("/auth", authRoutes);
 
